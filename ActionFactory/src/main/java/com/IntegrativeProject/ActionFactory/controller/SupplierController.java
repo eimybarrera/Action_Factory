6a5c7c1b644914da287953f963520557e7bd772d
davhx03 <daherreran@unal.edu.co>
2024-07-16 12:19:33 -0500

    [Feature][Create-Supplier-controller-service-and-repository][Classes SupplierController, SupplierService and SupplierRepository were created]

---
+++ ActionFactory/src/main/java/com/IntegrativeProject/ActionFactory/controller/SupplierController.java
@@ -0,0 +1,48 @@
+package com.IntegrativeProject.ActionFactory.controller;
+
+import com.IntegrativeProject.ActionFactory.model.Supplier;
+import com.IntegrativeProject.ActionFactory.service.SupplierService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/suppliers")
+public class SupplierController {
+    private SupplierService supplierService;
+
+    @Autowired
+    public SupplierController(SupplierService supplierService){
+        this.supplierService = supplierService;
+    }
+    @PostMapping()
+    public ResponseEntity<String> createSupplier(@RequestBody Supplier supplier){
+        this.supplierService.createSupplier(supplier);
+        return ResponseEntity.ok("Supplier created succesfully");
+    }
+
+    @GetMapping("{id}")
+    public Supplier getSupplierById(@PathVariable("id") int id){
+        return this.supplierService.getSupplierById(id);
+    }
+
+    @GetMapping()
+    public List<Supplier> getAllSuppliers(){return this.supplierService.getAllSuppliers();}
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<String> deleteSupplier(@PathVariable("id") int id){
+        this.supplierService.deleteSupplier(id);
+        return ResponseEntity.ok("Supplier deleted successfully");
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<Supplier> updateSupplier(@PathVariable("id") int id, @RequestBody Supplier supplier){
+        supplier.setId(id);
+        Supplier updatedSupplier = this.supplierService.updateSupplier(supplier);
+        return ResponseEntity.ok(updatedSupplier);
+    }
+
+}
+
